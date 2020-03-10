e6655a636d5b52c2b1aba37b9e6bd5eb8da12eec
alifele <ali.fele@gmail.com>
2020-03-10 22:01:59 +0330

    datasheet update

---
+++ Arduino Codes/PWM_with_MOSFET/PWM_with_MOSFET.ino
@@ -0,0 +1,17 @@
+int led = 6;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i=0; i<255; i++){
+    analogWrite(led, i);
+    delay(5);
+  }
+   for(int i=255; i>0; i--){
+    analogWrite(led, i);
+    delay(5);
+  }
+}
